baca4192214cfa993b60456a3cfc06d5139097d6
shoruki95 <58976258+shoruki95@users.noreply.github.com>
2020-01-05 17:09:30 +0100

    Update test_partie_1.py

--- jeu_threes/tests_unitaires/test_partie_1.py
+++ jeu_threes/tests_unitaires/test_partie_1.py
@@ -14,6 +14,7 @@ import ui.play_display
 #########################
 #      module play      #
 #########################
+
 def test_init_play():
     plateau=init_play()
 
@@ -26,43 +27,59 @@ def test_init_play():
     assert not(plateau['tiles'] == [0]) , "Erreur F"
     print("Fonction init_play : OK")
 
-###########################
-#  module : tiles_acces   #
-###########################    
+#########################
+#  module tiles_acces   #
+######################### 
+
 def test_check_indice():
+    p={'n':4,'nb_cases_libres':16,'tiles':[0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3]}
     assert check_indice(p,3)==True, "Erreur A"
     assert check_indice(p,5)==False, "Erreur B"
     assert not check_indice(p,6)==True, "Erreur C"
     print("Fonction check_indice: OK")
     
 def test_check_room():
-    assert check_room(p,5,6)==False, "Erreur A"
-    assert check_room(p,1,1)==True, "Erreur B"
-    assert check_room(p,2,5)==False, "Erreur C"
-    assert not check_room(p,5,0)==True, "Erreur D"
+    p={'n':4,'nb_cases_libres':16,'tiles':[0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3]}
+    assert check_room(p,2,1)==False, "Erreur A"
+    assert check_room(p,0,0)==True, "Erreur B"
+    assert check_room(p,0,2)==False, "Erreur C"
+    assert not check_room(p,3,1)==True, "Erreur D"
     print("Fonction check_room : OK")
 
 def test_get_value():
+    p={'n':4,'nb_cases_libres':16,'tiles':[0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3]}
     assert get_value(-1,10)==False, "Erreur A"
     assert get_value(p,8,0)==False, "Erreur B"
     assert get_value(p,1,8)==False, "Erreur C"
-    assert get_value(p,3,2)==0, "Erreur D"
-    assert get_value(p,0,2)==0, "Erreur E"
+    assert get_value(p,0,0)==0, "Erreur D"
+    assert get_value(p,2,3)==3, "Erreur E"
     print("Fonction get_value : OK")
-
+         
 def test_set_value():
-    assert  set_value(1,2)==True, "Erreur A"
-    assert set_value(0,8)==False, "Erreur B"
-    assert not set_value(8,3)==True, "Erreur C"
-    assert set_value(10,9)==False, "Erreur D"
+    p={'n':4,'nb_cases_libres':16,'tiles':[0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3]}
+    set_value(p,0,0,3)
+    assert p['tiles'][4*0+0]==3, "Erreur A"
+    set_value(p,3,0,1)
+    assert p['tiles'][4*3+0]==1, "Erreur B"
+    set_value(p,2,1,0)
+    assert p['tiles'][4*2+1]==0, "Erreur C"
     print("Fonction set_value : OK")
-
+          
 def test_is_room_empty():
-    assert is_room_empty(p,1,1)==True, "Erreur A"
+    p={'n':4,'nb_cases_libres':16,'tiles':[0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3]}
+    assert is_room_empty(p,0,0)==True, "Erreur A"
     assert not is_room_empty(p,1,2)==False, "Erreur B"
     assert is_room_empty(p,3,0)==True, "Erreur C"
     print("Fonction is_room_empty : OK")
     
+#########################
+#  module tiles_moves   #
+#########################
+
+#########################
+#      module play      #
+#########################
+
 def test_is_game_over():
     p1={'n':4,'nb_cases_libres':16,'tiles':[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]}
     assert is_game_over(p1)==True,"Erreur A"
@@ -80,13 +97,5 @@ def test_get_score():
     p3={'n':4,'nb_cases_libres':16,'tiles':[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]}
     assert not(get_score(p1)==0), "Erreur C"
     print("Fonction get_score : OK")
-    
-###########################
-#  module : play_dsplay   #
-###########################
 
-def test_affichage_moyen():
-    p1={'n':4,'nb_cases_libres':16,'tiles':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}
-    assert affichage_moyen(p1)==True,"Erreur A"
-    assert not(affichage_moyen(p1))==False,"Erreur B"
-    print("Fonction affichage_moyen : OK")
+    
