4b7257233751963b27be9fe6fa52aa6f361e37d6
CelestialTios <58608918+CelestialTios@users.noreply.github.com>
2020-01-05 15:33:40 +0100

    Update test_partie_1.py

--- jeu_threes/tests_unitaires/test_partie_1.py
+++ jeu_threes/tests_unitaires/test_partie_1.py
@@ -1,9 +1,13 @@
-﻿from play import*
-
+﻿from tiles.games.play import*
+from  
 def test_init_play():
     plateau=init_play()
 
     assert plateau['n']== 4 ,       "Erreur A"
     assert not(plateau['n']== 5) ,  "Erreur B"
-    assert plateau['nb_cases_libres']== 4 ,"Erreur A"
-    
+    assert plateau['nb_cases_libres']== 16 ,      "Erreur C"
+    assert not(plateau['nb_cases_libres']== 14) , "Erreur D"
+    assert plateau['tiles'] == [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] , "Erreur E"
+    assert not(plateau['tiles'] == [2,0,0,3,0,0,0,0,4,0,0]) , "Erreur F"
+    assert not(plateau['tiles'] == [0]) , "Erreur F"
+    print("Fonction init_play : OK")
