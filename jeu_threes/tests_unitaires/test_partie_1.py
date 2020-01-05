4c3a6be63864471fb58d8be68340324d8fd720b9
CelestialTios <58608918+CelestialTios@users.noreply.github.com>
2020-01-05 16:28:56 +0100

    Update test_partie_1.py

--- jeu_threes/tests_unitaires/test_partie_1.py
+++ jeu_threes/tests_unitaires/test_partie_1.py
@@ -7,8 +7,13 @@ import tiles.tiles_moves
 import game.play
 import ui.play_display
 
-p={'n':4,'nb_cases_libres':16,'tiles':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}
 
+########################################################
+########################################################
+
+#########################
+#      module play      #
+#########################
 def test_init_play():
     plateau=init_play()
 
@@ -20,3 +25,22 @@ def test_init_play():
     assert not(plateau['tiles'] == [2,0,0,3,0,0,0,0,4,0,0]) , "Erreur F"
     assert not(plateau['tiles'] == [0]) , "Erreur F"
     print("Fonction init_play : OK")
+
+def test_is_game_over():
+    p1={'n':4,'nb_cases_libres':16,'tiles':[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]}
+    assert is_game_over(p1)==True,"Erreur A"
+    p2={'n':4,'nb_cases_libres':16,'tiles':[1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}
+    assert not(is_game_over(p2))==True, "Erreur B"
+    p3={'n':4,'nb_cases_libres':16,'tiles':[0,0,0,0,0,0,0,0,0,0,0,0]}
+    assert not(is_game_over(p3))==True, "Erreur B"
+    print("Fonction is_game_over : OK")
+
+def test_get_score():
+    p1={'n':4,'nb_cases_libres':16,'tiles':[1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2]}
+    assert get_score(p1)==16, "Erreur A"
+    p2={'n':4,'nb_cases_libres':16,'tiles':[3,6,6,24,6,12,2,3,2,1,3,6,1,2,2,1]}
+    assert get_score(p1)==80, "Erreur B"
+    p3={'n':4,'nb_cases_libres':16,'tiles':[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]}
+    assert not(get_score(p1)==0), "Erreur C"
+    print("Fonction get_score : OK")
+    
