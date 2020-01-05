45d47ca0dfb4c1f7786505915c51ccdd6f1731e5
CelestialTios <58608918+CelestialTios@users.noreply.github.com>
2020-01-05 15:25:27 +0100

    Add files via upload

---
+++ jeu_threes/tests_unitaires/test_partie_1.py
@@ -0,0 +1,8 @@
+﻿from play import*
+
+def test_init_play():
+    plateau=init_play()
+
+    assert plateau['n']== 4 ,       "Erreur A"
+    assert not(plateau['n']== 5) ,  "Erreur B"
+    assert plateau['nb_cases_libres']== 4 ,"Erreur A"
