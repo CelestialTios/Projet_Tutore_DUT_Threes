6c7426d8ecb18279e72a74d2299787727d866f36
CelestialTios <58608918+CelestialTios@users.noreply.github.com>
2020-01-06 11:56:09 +0100

    Update tiles_moves.py

--- JustePourTravailler/tiles_moves.py
+++ JustePourTravailler/tiles_moves.py
@@ -34,25 +34,6 @@ def get_nb_empty_rooms(p):
 #  Fonction de la partie 2  #
 #############################
 
-###
-###
-
-def init_play():
-    """
-    Retourne un dictionnaire possédant trois clés correspondant respectivement à:
-    la taille n*n du plateau,
-    au nombre de cases libres,
-    aux tuiles.
-    """
-
-    p={}
-    p['n']=4
-    p['nb_cases_libres']=16
-    p['tiles']=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    return p
-
-###
-###
 
 def get_next_alea_tiles(plateau,mode):
     """
@@ -62,38 +43,41 @@ def get_next_alea_tiles(plateau,mode):
     @param-plateau: dictionnaire contenant les informations du plateau
     @param-mode: cast(string), choisit le nombre de tuiles donner
     """
-    tuiles={}
+    next_tiles={}
     if mode =="init":
-        j=2
-        tuiles['mode']='init'
-        next_tiles=tuiles(plateau,mode)
-        tuiles['0']=next_tiles
-        #tuiles['1']=tuiles(plateau,mode)
-    elif mode=="encours":
-        j=1
-        #tuiles['mode']='encours'
-        #tuiles['0']=tuiles(plateau,mode)
         
-    i=0
-    while i<j:
+        next_tiles['mode']='init'
         
-        i+=1
-
-    return tuiles
-
-def tuiles(plateau,mode):
-    """
-
-    """
-    next_tuiles={}
-    if mode=="init":
-        n=2
+        a1=tiles(plateau,2)
+        next_tiles['0']=a1
+        next_tiles['0']['val']=1
+        
+        a2=tiles(plateau,2)
+        next_tiles['1']=a2
+        next_tiles['1']['val']=2
+        
     elif mode=="encours":
-        n=3
+        
+        next_tiles['mode']='encours'
+        
+        a3=tiles(plateau,3)
+        next_tiles['0']=a3
+        
+    if is_game_over(plateau):
+        next_tiles['Check']=False
+    else:
+        next_tiles['Check']=True
+    return next_tiles
 
-    next_tuiles['val']=randint(1,n)
-    next_tuiles['lig'],next_tuiles['col']=0,0#position(plateau)
-    return next_tuiles
+
+def tiles(plateau,n):
+    
+    tiles={}
+
+    tiles['val']=randint(1,n)
+    tiles['lig'],tiles['col']=position(plateau)
+    return tiles
+
 
 def position(plateau):
     """
@@ -111,5 +95,18 @@ def position(plateau):
         is_room_empty(plateau,lig,col)
     return lig,col
 
-plateau=init_play()
-print(get_next_alea_tiles(plateau,'init'))
+
+def put_next_tiles(plateau,tiles):
+    
+    if tiles['mode']=='init':
+        
+        plateau['tiles'][4*tiles['0']['lig']+1*tiles['0']['col']]=tiles['0']['val']
+        if is_room_empty(plateau,tiles['1']['lig'],tiles['1']['col']):
+            
+            new_lig,new_col=position(plateau)
+            plateau['tiles'][4*new_lig+1*new_col]=tiles['1']['val']
+            
+    if tiles['mode']=='encours':
+        plateau['tiles'][4*tiles['0']['lig']+1*tiles['0']['col']]=tiles['0']['val']
+
+    
