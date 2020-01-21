2d1ff78ffea55d1f758d5b76338bb8b7ec9b4608
CelestialTios <58608918+CelestialTios@users.noreply.github.com>
2020-01-21 14:45:36 +0100

    Update user_entries.py

--- jeu_threes/ui/user_entries.py
+++ jeu_threes/ui/user_entries.py
@@ -1,3 +1,7 @@
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath +'/../')
+
 def get_user_move():
     print("Quelle est votre action ?")
     print("\t -'h' pour Haut")
