e750623987cf095733e05e43f19175d87b6c3d1c
CelestialTios <58608918+CelestialTios@users.noreply.github.com>
2020-01-21 14:42:11 +0100

    Add files via upload

---
+++ jeu_threes/app.py
@@ -0,0 +1,38 @@
+﻿from user_entries import *
+from cycle_game import *
+from tiles_acces import *
+from tiles_moves import *
+from play_display import *
+from play import *
+import sys
+
+def threes():
+    """
+    Permet de jouer des parties de Threes, de reprendre une partie sauvegardée
+    ou sauvegarder une partie en cours.
+    Tout ceci de façon répétée.
+    """
+    choix=get_user_menu()
+
+    while choix !='Q':
+        if choix=='N' or choix=='C':        #Lancer une partie
+            if choix=='N':                      #Nouvelle partie
+                resultat=cycle_play(None)
+            else:                               #Partie chargée
+                resultat=cycle_play(partie)
+
+            if resultat:
+                print('Votre score est de',partie['score'])
+                partie=None
+                save_game(partie)
+
+        elif choix=='L':
+            partie=restore_game()
+            print("Votre partie a été chargée")
+
+        elif choix=='S':
+            save_game()
+            print("Votre partie est chargée")
+        choix=get_user_menu()
+
+    sys.exit()                              #Quitter le jeu
---
+++ jeu_threes/game/play.py
@@ -0,0 +1,57 @@
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath +'/../')
+
+
+import tiles.tiles_moves
+
+#############################
+#  Fonction de la partie 1  #
+#############################
+
+def init_play():
+    """
+    Retourne un dictionnaire possédant trois clés correspondant respectivement à:
+    la taille n*n du plateau,
+    au nombre de cases libres,
+    aux tuiles.
+    """
+
+    p={}
+    p['n']=4
+    p['nb_cases_libres']=16
+    p['tiles']=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    return p
+
+def is_game_over(p):
+    """
+    Retourne True si la partie est terminée
+    Utilise la fonction get_nb_empty_rooms
+    pour vérifier le nombre de case disponible
+
+    @param-p:dictionnaire contenant les informations du plateau
+    """
+    if get_nb_empty_rooms(p)==0:
+        return True
+
+    return False
+
+
+
+def get_score(p):
+    """
+    Retourne le score du plateau
+
+    @param-p:dictionnaire contenant les informations du plateau
+    """
+    score=0
+    i=0
+    while i < len(p['tiles']):
+        score += p['tiles'][i]
+        i += 1
+    return score
+
+#############################
+#  Fonction de la partie 2  #
+#############################
+
